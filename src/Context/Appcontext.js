fb535765a6e7cb7cda772872ef318ffad15111b7
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-08-19 21:41:17 +0500

    updated version

--- src/Context/Appcontext.js
+++ src/Context/Appcontext.js
@@ -1,12 +1,14 @@
 import React,{createContext, useReducer} from 'react';
 import Appreducer from './Appreducer'
+
+/* saved transaction list  */
 const initialtransaction ={
     transactions:
     [
     {id:1 ,   amount: 500, text: "Cash" },
     {id:2 ,   amount: -50, text: "Cold Drink" },
     {id:3 ,   amount: 100, text: "Deposit" },
-    {id:4 ,   amount: -200, text: "Utility Bill" },
+    {id:4 ,   amount: -200, text: "Books" },
 ]
 }
 export const Transactioncontext = createContext(initialtransaction);
